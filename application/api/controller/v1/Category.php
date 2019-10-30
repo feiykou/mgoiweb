bf13fee59428eab27a15e642d5a1e119ce9bd160
feiy <feiy@163.com>
2019-10-30 22:16:37 +0800

    唔该

--- application/api/controller/v1/Category.php
+++ application/api/controller/v1/Category.php
@@ -5,6 +5,7 @@ namespace app\api\controller\v1;
 
 
 use app\api\controller\BaseController;
+use app\api\validate\CategoryResc;
 use app\common\model\Category as CategoryModel;
 use app\api\validate\Category as CategoryValidate;
 
@@ -19,15 +20,14 @@ class Category extends BaseController
      * @throws \app\lib\exception\ParameterException
      */
     public function getCate($times){
-        (new CategoryValidate())->goCheck('cate');
+        (new CategoryValidate())->goCheck();
         $pid = input('cate_id', 0, 'int');
         $proCateData = CategoryModel::getCateJson('type, link_url, main_img_url, mobile_imgs_url, description', $times, $pid);
         return json($proCateData);
     }
 
-
     public function getCategoryData($cate_id, $attr_id, $type_id) {
-        (new CategoryValidate())->goCheck('resc');
+        (new CategoryResc())->goCheck();
         $data = [];
         switch ($type_id){
             case 1:
