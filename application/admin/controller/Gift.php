ebba7b75de2f8b97eb55d9f8031510cd2c119d89
feiy <feiy@163.com>
2019-11-13 15:53:51 +0800

    唔该

--- application/admin/controller/Gift.php
+++ application/admin/controller/Gift.php
@@ -36,7 +36,7 @@ class Gift extends Base
         $CategoryRes=$Category->Catetree($CategoryRes);
 
         // 获取全部商品
-        $proAllData = model('product')->getAllProData(1);
+        $proAllData = model('product')->getAllProData();
         $attrData = config('attributes.gift_resc_type');
         $labelsData = config('attributes.labels_type');
         $this->assign([
